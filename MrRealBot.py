c7270768dea8c83c9fd22bb9d725d5228d121c89
xhico <31449326+xhico@users.noreply.github.com>
2021-01-31 00:41:54 +0000

    Update MrRealBot.py

--- MrRealBot.py
+++ MrRealBot.py
@@ -1,102 +1,70 @@
 # -*- coding: utf-8 -*-
 # !/usr/bin/python3
 
-# pip3 install tweepy animals.py schedule
+# pip3 install tweepy
 
 import os
-import pytz
 import time
 import tweepy
-import schedule
-import urllib.request
-from random import randint
-from animals import Animals
-from datetime import datetime
+from dotenv import load_dotenv
+load_dotenv()
+
+debug = True
 
 
 def oauthLogin():
-    CONSUMER_KEY = ''
-    CONSUMER_SECRET = ''
-    ACCESS_TOKEN = ''
-    ACCESS_TOKEN_SECRET = ''
+    CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
+    CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
+    ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+    ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
 
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     api = tweepy.API(auth)
-    
-    print("Authenticated as: %s" % api.me().screen_name)
-    
-    return api
-      
 
-def tweet(api, str, img, replyId):
+    if debug:
+        print("Authenticated as: %s" % api.me().screen_name)
+
+    return api
+
+
+def tweet(api, str):
     try:
-        if img != "":
-            api.update_with_media(img, str)
-        elif replyId != 0:
-            api.update_status('Have a nice day!', replyId, auto_populate_reply_metadata = True)
-        else:
-            api.update_status(str)
-        print("Tweeted")
-        
-    except Exception as inst:
-        print("Failed to Tweet")
-        print(inst)
+        api.update_status(str)
+        if debug:
+            print("Tweeted - " + str)
+    except:
+        if debug:
+            print("Failed to Tweet - " + str)
         return False
-    
+
     return True
-    
-    
+
+
 def batchDelete(api):
     for tw in tweepy.Cursor(api.user_timeline).items():
         try:
-            print("Deleting ID: " + str(tw.id) + " - ", end =" ")
+            if debug:
+                print("Deleting ID: " + str(tw.id) + " - ", end=" ")
             api.destroy_status(tw.id)
-            print("Deleted")
-        except Exception as inst:
-            print("Failed to Delete")
-    
+            if debug:
+                print("Deleted")
+        except:
+            if debug:
+                print("Failed to Delete")
+
     return True
 
 
-def getAnimal(dirName):
-    animal = Animals('cat')
-    imgUrl = animal.image()
-    imgFact = animal.fact()
-    imgFilename = os.path.join(dirName, str(randint(1, 999)) + ".jpg")
-    
-    urllib.request.urlretrieve(imgUrl, imgFilename)
+def measure_temp():
+    temp = os.popen("vcgencmd measure_temp").readline().replace("temp=", "").rstrip()
+    return temp
 
-    return imgFilename
-    
-
-def deleteJPG(dirName):
-    for file in os.listdir(dirName):
-        if file.endswith(".jpg"):
-            os.remove(file)
-    
-    return True
-
-
-def getLastTweet(api, userName):
-    tweet = api.user_timeline(screen_name = userName, count = 1, include_rts = False, exclude_replies = True)[0]
-    now = datetime.now()
-    if str(tweet.created_at) < str(now.strftime("%Y-%m-%d %H:%M:%S")):
-        return tweet.id
-    
-    return 0
-    
 
 if __name__ == "__main__":
-    myDir = os.getcwd()
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
     myAPI = oauthLogin()
-    
-    tweet(myAPI, "Hallo", "", 0)
-    #batchDelete(myAPI)
-    #deleteJPG(myDir)
-    
 
-    
-    
-    
-    
+    batchDelete(myAPI)
+    # piTemp = "Raspberry Pi Temperature: " + measure_temp()
+    # tweet(myAPI, piTemp)
