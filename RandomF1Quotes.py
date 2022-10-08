43c2def5b28c21b1581e425f9565a0af3d8cce22
xhico <francgf@gmail.com>
2022-10-08 16:51:48 +0100

    Logging

--- RandomF1Quotes.py
+++ RandomF1Quotes.py
@@ -4,6 +4,7 @@
 # python3 -m pip install tweepy yagmail pandas python-dateutil psutil --no-cache-dir
 import datetime
 import json
+import logging
 import os
 import random
 import psutil
@@ -19,32 +20,18 @@ def get911(key):
     return data[key]
 
 
-CONSUMER_KEY = get911('TWITTER_F1_CONSUMER_KEY')
-CONSUMER_SECRET = get911('TWITTER_F1_CONSUMER_SECRET')
-ACCESS_TOKEN = get911('TWITTER_F1_ACCESS_TOKEN')
-ACCESS_TOKEN_SECRET = get911('TWITTER_F1_ACCESS_TOKEN_SECRET')
-EMAIL_USER = get911('EMAIL_USER')
-EMAIL_APPPW = get911('EMAIL_APPPW')
-EMAIL_RECEIVER = get911('EMAIL_RECEIVER')
-
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
-
-
 def tweet(tweetStr):
     try:
         api.update_status(tweetStr)
-        print("Tweeted")
+        logger.info("Tweeted")
         return True
     except Exception as ex:
-        print(ex)
-        print("Failed")
+        logger.error(ex)
     return False
 
 
 def getRandomQuote():
-    csvFile = "Quotes.csv"
+    csvFile = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Quotes.csv")
     df = pd.read_csv(csvFile)
     idx = random.randint(0, len(df))
 
@@ -53,7 +40,7 @@ def getRandomQuote():
 
 
 def favTweets(tags, numbTweets):
-    print("favTweets")
+    logger.info("favTweets")
     tags = tags.replace(" ", " OR ")
     tweets = tweepy.Cursor(api.search_tweets, q=tags).items(numbTweets)
     tweets = [tw for tw in tweets]
@@ -84,19 +71,34 @@ def main():
 
 
 if __name__ == "__main__":
-    print("----------------------------------------------------")
-    print(str(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")))
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    
+    # Set Logging
+    LOG_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.path.abspath(__file__).replace(".py", ".log"))
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", handlers=[logging.FileHandler(LOG_FILE), logging.StreamHandler()])
+    logger = logging.getLogger()
+
+    logger.info("----------------------------------------------------")
+
+    CONSUMER_KEY = get911('TWITTER_F1_CONSUMER_KEY')
+    CONSUMER_SECRET = get911('TWITTER_F1_CONSUMER_SECRET')
+    ACCESS_TOKEN = get911('TWITTER_F1_ACCESS_TOKEN')
+    ACCESS_TOKEN_SECRET = get911('TWITTER_F1_ACCESS_TOKEN_SECRET')
+    EMAIL_USER = get911('EMAIL_USER')
+    EMAIL_APPPW = get911('EMAIL_APPPW')
+    EMAIL_RECEIVER = get911('EMAIL_RECEIVER')
+
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
+
     # Check if script is already running
     procs = [proc for proc in psutil.process_iter(attrs=["cmdline"]) if os.path.basename(__file__) in '\t'.join(proc.info["cmdline"])]
     if len(procs) > 2:
-        print("isRunning")
+        logger.info("isRunning")
     else:
         try:
             main()
         except Exception as ex:
-            print(traceback.format_exc())
+            logger.error(traceback.format_exc())
             yagmail.SMTP(EMAIL_USER, EMAIL_APPPW).send(EMAIL_RECEIVER, "Error - " + os.path.basename(__file__), str(traceback.format_exc()))
         finally:
-            print("End")
+            logger.info("End")
