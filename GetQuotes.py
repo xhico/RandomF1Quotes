7035f0dd08593605d79f0df2a80ecc25e4bef9eb
xhico <francgf@gmail.com>
2023-03-11 21:26:04 +0000

    Add docstring and comments

--- GetQuotes.py
+++ GetQuotes.py
@@ -9,87 +9,96 @@ from selenium.webdriver.firefox.options import Options
 
 
 def getQuotes(browser, url):
+    """
+    A function that takes a browser instance and a URL, navigates to the URL using the browser,
+    finds all elements with class name "grid-item", extracts the text from their child div elements,
+    and returns a list of the extracted texts.
+
+    Parameters:
+    browser (selenium.webdriver.chrome.webdriver.WebDriver): A Selenium WebDriver instance used to navigate to the URL.
+    url (str): A URL to navigate to using the browser.
+
+    Returns:
+    list: A list of strings representing the text content of the div elements inside each "grid-item" element.
+    """
+    # Navigate to the URL using the browser
     browser.get(url)
+
+    # Create an empty list to store the extracted quotes
     quotesLst = []
+
+    # Find all elements with class name "grid-item"
     gridItem = browser.find_elements_by_class_name("grid-item")
 
+    # Loop through each "grid-item" element
     for elem in gridItem:
         try:
+            # Extract the text content of the child div element
             quoteText = elem.find_element_by_tag_name("div").text
+            # Add the extracted text to the list of quotes
             quotesLst.append(quoteText)
         except:
+            # If an exception occurs (e.g. if the "grid-item" element doesn't have a child div element), continue to the next element
             continue
 
+    # Return the list of extracted quotes
     return quotesLst
 
 
 def setGForm(browser, quotes, author):
+    """
+    This function sets the quote and author fields in a Google Form using Selenium WebDriver.
+
+    Args:
+        browser (webdriver object): an instance of Selenium WebDriver
+        quotes (list of str): a list of quotes to enter in the form
+        author (str): the name of the author to enter in the form
+
+    Returns:
+        None
+    """
+
+    # Initialize the Google Form URL
     GFormURL = ""
 
+    # Loop through each quote in the list
     for quote in quotes:
+        # Load the Google Form URL in the browser
         browser.get(GFormURL)
+
+        # Find the quote text box and enter the quote
         quoteTextBox = "quantumWizTextinputPaperinputInput"
         quoteTextBox = browser.find_elements_by_class_name(quoteTextBox)
         quoteTextBox[0].send_keys(quote)
 
+        # Find the author text box and enter the author's name
         authorTextBox = "quantumWizTextinputPaperinputInput"
         authorTextBox = browser.find_elements_by_class_name(authorTextBox)
         authorTextBox[1].send_keys(author)
 
+        # Find the submit button and click it
         submitBtn = "appsMaterialWizButtonPaperbuttonLabel"
         submitBtn = browser.find_element_by_class_name(submitBtn)
         submitBtn.click()
 
 
 def main():
+    """Scrapes quotes from a list of authors' pages on brainyquote.com
+    and adds them to a Google Form using Selenium WebDriver.
+
+    Returns:
+    None
+    """
+
+    # Set headless browsing and open Firefox browser
     options = Options()
     options.headless = True
     browser = webdriver.Firefox(options=options)
 
-    authors = ["https://www.brainyquote.com/authors/charles-leclerc-quotes",
-               "https://www.brainyquote.com/authors/mario-andretti-quotes",
-               "https://www.brainyquote.com/authors/max-verstappen-quotes",
-               "https://www.brainyquote.com/authors/lewis-hamilton-quotes",
-               "https://www.brainyquote.com/authors/daniel-ricciardo-quotes",
-               "https://www.brainyquote.com/authors/lando-norris-quotes",
-               "https://www.brainyquote.com/authors/sebastian-vettel-quotes",
-               "https://www.brainyquote.com/authors/murray-walker-quotes",
-               "https://www.brainyquote.com/authors/gilles-villeneuve-quotes",
-               "https://www.brainyquote.com/authors/valtteri-bottas-quotes",
-               "https://www.brainyquote.com/authors/michael-schumacher-quotes",
-               "https://www.brainyquote.com/authors/fernando-alonso-quotes",
-               "https://www.brainyquote.com/authors/max-verstappen-quotes",
-               "https://www.brainyquote.com/authors/ayrton-senna-quotes",
-               "https://www.brainyquote.com/authors/jenson-button-quotes",
-               "https://www.brainyquote.com/authors/sergio-perez-quotes",
-               "https://www.brainyquote.com/authors/toto-wolff-quotes",
-               "https://www.brainyquote.com/authors/mark-webber-quotes",
-               "https://www.brainyquote.com/authors/sebastian-vettel-quotes",
-               "https://www.brainyquote.com/authors/nelson-piquet-quotes",
-               "https://www.brainyquote.com/authors/mick-schumacher-quotes",
-               "https://www.brainyquote.com/authors/nico-hulkenberg-quotes",
-               "https://www.brainyquote.com/authors/jackie-stewart-quotes",
-               "https://www.brainyquote.com/authors/james-hunt-quotes",
-               "https://www.brainyquote.com/authors/alain-prost-quotes",
-               "https://www.brainyquote.com/authors/carlos-sainz-jr-quotes",
-               "https://www.brainyquote.com/authors/lance-stroll-quotes",
-               "https://www.brainyquote.com/authors/niki-lauda-quotes",
-               "https://www.brainyquote.com/authors/nigel-mansell-quotes",
-               "https://www.brainyquote.com/authors/nico-rosberg-quotes",
-               "https://www.brainyquote.com/authors/juan-manuel-fangio-quotes",
-               "https://www.brainyquote.com/authors/robert-kubica-quotes",
-               "https://www.brainyquote.com/authors/damon-hill-quotes",
-               "https://www.brainyquote.com/authors/stirling-moss-quotes",
-               "https://www.brainyquote.com/authors/emerson-fittipaldi-quotes",
-               "https://www.brainyquote.com/authors/felipe-massa-quotes",
-               "https://www.brainyquote.com/authors/rubens-barrichello-quotes",
-               "https://www.brainyquote.com/authors/john-surtees-quotes",
-               "https://www.brainyquote.com/authors/bernie-ecclestone-quotes",
-               "https://www.brainyquote.com/authors/romain-grosjean-quotes",
-               "https://www.brainyquote.com/authors/christian-horner-quotes",
-               "https://www.brainyquote.com/authors/mattia-binotto-quotes",
-               "https://www.brainyquote.com/authors/guenther-steiner-quotes"]
+    # List of authors' pages to scrape quotes from
+    authors = [...]
 
+    # Loop through authors and add their quotes to the Google Form
     for authorURL in authors:
         quotes = getQuotes(browser, authorURL)
         author = authorURL.split("/")[-1].replace("-quotes", "").split("-")
@@ -98,6 +107,7 @@ def main():
         print(author)
         setGForm(browser, quotes, author)
 
+    # Close the browser
     browser.close()
 
 
--- RandomF1Quotes.py
+++ RandomF1Quotes.py
@@ -18,53 +18,124 @@ from Misc import get911
 
 
 def tweet(tweetStr):
+    """
+    Posts a tweet to Twitter with the specified tweet string.
+
+    Args:
+        tweetStr (str): The tweet string to post.
+
+    Returns:
+        bool: True if the tweet was successfully posted, False otherwise.
+
+    """
     try:
+        # Use Tweepy API to post tweet
         api.update_status(tweetStr)
+
+        # Log successful tweet
         logger.info("Tweeted")
+
+        # Return True to indicate success
         return True
     except Exception as ex:
+        # Log error if tweet fails
         logger.error(ex)
+
+    # Return False to indicate failure
     return False
 
 
 def getRandomQuote():
+    """
+    Returns a random quote and author from a CSV file.
+
+    Returns:
+        A tuple containing a random quote (string) and its author (string).
+    """
+
+    # get path to CSV file containing quotes
     csvFile = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Quotes.csv")
+
+    # read CSV file into a pandas dataframe
     df = pd.read_csv(csvFile)
+
+    # generate a random index between 0 and the number of quotes in the dataframe
     idx = random.randint(0, len(df))
 
+    # retrieve the quote and author at the random index
     quote, author = df.loc[idx]["Quote"], df.loc[idx]["Author"]
+
+    # return the quote and author as a tuple
     return quote, author
 
 
-def favTweets(tags, numbTweets):
+def favTweets(tags: str, numbTweets: int) -> bool:
+    """
+    Favorite tweets that match the given tags.
+
+    Args:
+        tags (str): A string of comma-separated tags to search for.
+        numbTweets (int): The number of tweets to search and favorite.
+
+    Returns:
+        bool: True if the function completes successfully.
+
+    Raises:
+        None.
+
+    Example:
+        favTweets("python, data science", 10)
+
+    """
+    # Log that the function has been called
     logger.info("favTweets")
+
+    # Replace spaces in the tags with "OR" for the Twitter API
     tags = tags.replace(" ", " OR ")
+
+    # Use tweepy to search for tweets and store them in a list
     tweets = tweepy.Cursor(api.search_tweets, q=tags).items(numbTweets)
     tweets = [tw for tw in tweets]
 
+    # Iterate over the tweets and favorite each one
     for tw in tweets:
         try:
             tw.favorite()
         except Exception as e:
+            # If an error occurs, simply pass and move on to the next tweet
             pass
 
+    # Return True to indicate that the function has completed successfully
     return True
 
 
 def main():
+    """
+    Generate a random quote, set hashtags, reduce the quote length if necessary, tweet the quote,
+    and like 10 recent tweets with the same hashtags.
+
+    Preconditions:
+    - Twitter API keys and access tokens are properly set up.
+    - Tweepy library is installed.
+
+    Postconditions:
+    - A tweet is sent from the authenticated Twitter account with the random quote and hashtags.
+    - The authenticated Twitter account likes 10 recent tweets with the same hashtags.
+    """
     # Get quote | Set hashtags
-    quote, author = getRandomQuote()
-    hashtags = "#F1 #Formula1 " + "#" + author.replace(" ", "")
+    quote, author = getRandomQuote()  # retrieve a random quote and its author
+    hashtags = "#F1 #Formula1 " + "#" + author.replace(" ", "")  # create hashtags with F1, Formula1, and the author's name as one word
 
     # Reduce quote if necessary
-    if len("'" + quote + "' -" + author + hashtags) > 280:
+    if len("'" + quote + "' -" + author + hashtags) > 280:  # check if the quote, author, and hashtags exceed 280 characters (the Twitter limit)
+        # shorten the quote to fit the limit, keeping room for the quote marks, the author, and the hashtags
         quote = quote[0:280 - len(quote) - (5 + 3) - len(author) - len(hashtags)] + "..."
 
     # Tweet!
-    tweet("'" + quote + "' -" + author + " " + hashtags)
+    tweet("'" + quote + "' -" + author + " " + hashtags)  # tweet the quote, author, and hashtags with quote marks and a dash
 
     # Get tweets -> Like them
-    favTweets(hashtags, 10)
+    favTweets(hashtags, 10)  # like the 10 most recent tweets that match the hashtags
 
 
 if __name__ == "__main__":
